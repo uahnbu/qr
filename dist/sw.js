5cd1f2e83446119f1da403fc09daa167bef9c753
uahnbu <uahnbu29main@gmail.com>
2021-09-01 20:53:42 +0700

    🐛 Fix builder destination path 💡 Remove unnecessary comment

--- dist/sw.js
+++ dist/sw.js
@@ -38,7 +38,7 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch event for', event.request.url);
+  // console.log('Fetch event for', event.request.url);
   event.respondWith(
     caches.match(event.request).then(response => {
       if (response) return response;
